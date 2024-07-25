0dfa9b6eb47504beb2435be51f38f5f1c29bed93
Toribi <68600284+Toribi@users.noreply.github.com>
2024-07-24 22:11:24 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
-let socket = new WebSocket('ws://' + window.location.host);
+let socket = new WebSocket('ws://' + window.location.host + ':10000');
 
 socket.onopen = function(event) {
-    console.log('Connected to the server');
+    console.log('WebSocket connection opened.');
+};
+
+socket.onerror = function(error) {
+    console.error('WebSocket Error: ', error);
 };
 
 socket.onmessage = function(event) {
     let data = JSON.parse(event.data);
+    console.log('Message from server: ', data);
     document.getElementById('result').innerText = data.result;
 };
 
@@ -13,5 +18,6 @@ function makeMove(move) {
     let message = {
         move: move
     };
+    console.log('Sending move: ', message);
     socket.send(JSON.stringify(message));
 }
